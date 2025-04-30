35f7a4dcc094a2b41549a7face48fd9f878493fb
Lee-Ann <leeann.seegets@gmail.com>
2025-04-30 16:57:34 +0200

    add accelerometer

--- dippid_sender/receiver.py
+++ dippid_sender/receiver.py
@@ -1,6 +1,19 @@
 from DIPPID import SensorUDP
+from vis import Visualizer
+
 PORT = 5700
 sensor = SensorUDP(PORT)
+update_frequency = 10
+
+
+# Visualize accelerometer data (I borrowed the Visualizer from the lecture)
+
+def main():
+    visualizer = Visualizer(sensor=sensor, update_freq=update_frequency)   
+    visualizer.run()
+
+
+# Print button status
 
 def handle_btn(data):
     if data == 1:
@@ -8,4 +21,14 @@ def handle_btn(data):
     else:
         print('button released')
 
+
+# Print accelerometer data
+
+def handle_acc(data):
+    print(data)
+
 sensor.register_callback('button_1', handle_btn)
+#sensor.register_callback('accelerometer', handle_acc)
+
+if __name__ == '__main__':
+    main()
